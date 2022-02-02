88d45ae573d206212f5a4f44ffb86cde6bb3be59
taukir707 <taukir707@gmail.com>
2022-02-02 23:22:39 +0530

    s3 read and delete object

--- AWS/S3/s3_api.py
+++ AWS/S3/s3_api.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+from botocore.exceptions import ClientError
 
 class Clients3:
 
@@ -11,6 +12,33 @@ class Clients3:
 
         self.s3_client = boto3.client("s3", aws_access_key_id=Clients3.ACCESS_KESY,
                                       aws_secret_access_key=Clients3.SECRET_KEY)
+
+    def display_msg(self,status=1,msg = 'success',error = None):
+
+        return {"status":status,"msg" : msg, "error": error}
+
+
+
+    def create_bucket(self, bucket_name):
+
+        try:
+            response = self.s3_client.create_bucket(Bucket = bucket_name,ACL = 'public-read',CreateBucketConfiguration={'LocationConstraint':'ap-south-1'})
+            return self.display_msg(msg="Bucket created successfully")
+
+        except ClientError as e:
+            return self.display_msg(status =2,msg = "error",error=str(e))
+
+
+    def upload_object(self,file_name,bucket_name,key = None):
+
+        if key is None:
+            key = file_name.split('/')[-1]
+        with open(file_name, 'rb') as data:
+            self.s3_client.upload_fileobj(data, bucket_name, key)
+
+    def delete_object(self,bucket_name,key):
+        self.s3_client.delete_object(Bucket = bucket_name,Key = key)
+
 
     def get_objects(self):
 
@@ -56,7 +84,10 @@ class Clients3:
 
 if __name__ == "__main__":
     obj = Clients3()
-    list_obj = obj.get_objects()
-    resp = obj.row_count(list_obj)
-    print(resp)
+    # list_obj = obj.get_objects()
+    # resp = obj.row_count(list_obj)
+    # print(resp)
+    # print(obj.create_bucket(bucket_name ="bina147863"))
+    # obj.upload_object(file_name="/home/lenovo/Workforce/AMAZON/external_data/data/file1.txt",bucket_name='bina147863',key ="SALES/file1.txt")
+    obj.delete_object(bucket_name='bina147863',key='SALES/housing.csv')
 
---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+git access token : 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
+
